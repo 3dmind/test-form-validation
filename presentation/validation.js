b9dd209ff7a34f2898873f2ebe8abe63797b4973
Christian Wlatschiha <christian.wlatschiha@quantilope.com>
2019-10-16 13:53:24 +0200

    docs: Revised example for workshop presentation.

--- presentation/validation.js
+++ presentation/validation.js
@@ -2,41 +2,48 @@ const { Success, Failure, collect } = require('folktale/validation')
 
 function isNotEmpty(field, value) {
   return value.trim() === ''
-    ? Failure([{ [field]: 'Value must not be empty.'} ])
+    ? Failure([{ [field]: 'Value must not be empty.' }])
     : Success(value)
 }
 
-function isToShort(value) {
+function isToShort(field, value) {
   return value.length > 6
     ? Success(value)
-    : Failure(['Value is to short.'])
+    : Failure([{ [field]: 'Value is to short.' }])
 }
 
-function isPasswordValid(password) {
+function matches(field, value) {
+  return /\W/.test(value)
+    ? Success(value)
+    : Failure([{ [field]: 'Value must contain special character.' }])
+}
+
+function isPasswordValid(value) {
+  const field = 'password'
   return collect([
-      isNotEmpty('password',password),
-      isToShort(password),
+    isNotEmpty(field, value),
+    isToShort(field, value),
+  ])
+}
+
+function isUserNameValid(value) {
+  return collect([
+    isNotEmpty('userName', value),
+  ])
+}
+
+function validate(userNameValue, passwordValue) {
+  return collect([
+      isUserNameValid(userNameValue),
+      isPasswordValid(passwordValue),
     ],
-  )
+  ).map(() => [userNameValue, passwordValue])
 }
 
-function isUserNameValid(userName) {
-  return collect([
-    isNotEmpty(userName),
-  ])
-}
-
-function validate(userName, password) {
-  return collect([
-    isUserNameValid(userName),
-    isPasswordValid(password),
-  ]).map(() => [
-    userName,
-    password,
-  ])
-}
-
-validate('lisa', 'lisasimpson').matchWith({
-  Success: function () {},
-  Failure: function () {}
-}) //?
+const userName = 'lisa'
+const password = 'simpson'
+validate(userName, password) //?$.value
+  .matchWith({
+    Success: function() {},
+    Failure: function() {},
+  })
